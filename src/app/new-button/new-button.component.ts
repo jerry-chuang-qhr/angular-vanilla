784d1493765979b3014fd46a254788690dbb127b
jerry.chuang <jerry.chuang@qhrtech.com>
2024-09-27 14:25:40 -0700

    feat(*): add a new component with story

---
+++ src/app/new-button/new-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-button.component.html',
+  styleUrl: './new-button.component.scss'
+})
+export class NewButtonComponent {
+
+}
